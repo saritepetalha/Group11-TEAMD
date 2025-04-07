3deaeee5da5660930a12b11848f593bb974a39b0
emirgocen03 <emirgocen03@gmail.com>
2025-04-08 01:33:50 +0300

    32 by 32 random sprites are printed on the gamescreen

--- KUTowerDefense/src/main/Game.java
+++ KUTowerDefense/src/main/Game.java
@@ -1,19 +1,40 @@
 package main;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class Game extends JFrame {
 	
 	private GameScreen gamescreen;
+	private BufferedImage img;
 	
 	public Game() {
-		setSize(400, 400);
+		
+		importImg();
+		
+		setSize(640, 640);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		
-		gamescreen = new GameScreen();
+		gamescreen = new GameScreen(img);
 		add(gamescreen);
+		
+	}
+
+	private void importImg() {
+		
+		InputStream is = getClass().getResourceAsStream("/spriteatlas.png");
+		
+		try {
+			img = ImageIO.read(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
--- KUTowerDefense/src/main/GameScreen.java
+++ KUTowerDefense/src/main/GameScreen.java
@@ -2,19 +2,60 @@ package main;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
 public class GameScreen extends JPanel {
 	
-	public GameScreen() {
+	private Random random;
+	private BufferedImage img;
+	
+	private ArrayList<BufferedImage> sprites = new ArrayList<>();
+	
+	
+	public GameScreen(BufferedImage img) {
+		this.img = img;
+		
+		loadSprites();
+		
+		random = new Random();
 		
 	}
 	
+	private void loadSprites() {
+		for (int y = 0; y < 10; y++) {
+			for (int x = 0; x < 10; x++) {
+				sprites.add(img.getSubimage(x * 32, y * 32, 32, 32));		
+			}
+		}
+		
+	}
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.RED);
-		g.fillRect(0, 0, 60, 60);
+				
+		for (int y = 0; y < 20; y++) {
+			for (int x = 0; x < 20; x++) {
+				g.drawImage(sprites.get(getRandomInt()), x * 32, y * 32, null);
+			}		
+		}
+	}
+	
+	private int getRandomInt() {
+		return random.nextInt(100);
+	}
+	
+	private Color getRandomColor() {
+		int r = random.nextInt(256);
+		int g = random.nextInt(256);
+		int b = random.nextInt(256);
+		
+		return new Color(r, g, b);
+
+		
 	}
 
 }
