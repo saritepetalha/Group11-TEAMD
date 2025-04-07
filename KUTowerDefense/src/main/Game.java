d0bbe113217d251b7c9b56acd8a50fe761d4a3a5
emirgocen03 <emirgocen03@gmail.com>
2025-04-08 00:43:34 +0300

    GameScreen and Game classes created, initial window with a red box :)

--- KUTowerDefense/src/main/Game.java
+++ KUTowerDefense/src/main/Game.java
@@ -1,9 +1,26 @@
 package main;
 
-public class Game {
+import javax.swing.JFrame;
+
+public class Game extends JFrame {
+	
+	private GameScreen gamescreen;
+	
+	public Game() {
+		setSize(400, 400);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+		gamescreen = new GameScreen();
+		add(gamescreen);
+		
+	}
 
 	public static void main(String[] args) {
 		
+		System.out.println("HELLO");
+		Game game = new Game();
 
 	}
 
--- KUTowerDefense/src/module-info.java
+++ KUTowerDefense/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module KUTowerDefense {
+	requires java.desktop;
 }
